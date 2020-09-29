7b207db9841f7fbdb67cc908aa8c23466c0c297b
Allen DeHoff <allen.dehoff21@gmail.com>
2020-09-28 19:21:28 -0600

    added ability to view employees, view departments, view roles, or exit application

--- employeeManager.js
+++ employeeManager.js
@@ -21,6 +21,57 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     // run the start function after the connection is made to prompt the user;
-    connection.end()
+    // connection.end()
+    init()
 });
 
+const intialize = {
+    name: "route",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+        "View all employees.",
+        "View all departments.",
+        "View all roles.",
+        "Exit"
+    ]
+}
+
+function init() {
+    inquirer.prompt(intialize)
+        .then(function (response) {
+            if (response.route === "View all employees."){
+                viewEmployees()
+            } else if (response.route === "View all departments."){
+                viewDepartments()
+            } else if (response.route === "View all roles."){
+                viewRoles()
+            } else if (response.route --- "Exit."){
+                connection.end()
+            }
+        })
+}
+
+function viewEmployees(){
+    connection.query("SELECT * FROM employees", function (err, res) {
+        if (err) throw err
+        console.table(res)
+        init()
+    })
+}
+
+function viewDepartments(){
+    connection.query("SELECT * FROM departments", function (err, res) {
+        if (err) throw err
+        console.table(res)
+        init()
+    })
+}
+
+function viewRoles(){
+    connection.query("SELECT * FROM roles", function (err, res) {
+        if (err) throw err
+        console.table(res)
+        init()
+    })
+}
